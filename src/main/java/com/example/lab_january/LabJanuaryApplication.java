26831b0ec4698d3a520c87f11051223d9787f3fc
Tea <tea.temelkovska410@gmail.com>
2023-01-17 19:20:46 +0100

    Initial commit

---
+++ src/main/java/com/example/lab_january/LabJanuaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_january;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJanuaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabJanuaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab_january/LabJanuaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_january;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJanuaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
